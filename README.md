ef5aa1d4eb6aabf94a7ba0cb21f425aa85157bc1
Michael Stahnke <stahnke@floxdev.com>
2023-10-27 17:18:36 -0500

    Readying for release

---
+++ README.md
@@ -0,0 +1,20 @@
+# hubot-armadillo
+
+Spit random armadillo facts.
+
+
+
+# Inspiration
+https://armadillo-online.org/facts.html
+
+# Installation
+
+`npm install --save hubot-armadillo`
+
+Add 'hubot-armadillo to your `external-scripts` file. That's all.
+
+
+
+# License
+
+WTFPL
--- src/armadillo.js
+++ src/armadillo.js
@@ -10,13 +10,21 @@
 //   hubot armadillo  - displays a random Armadillo fact
 // 
 
+
 /*jshint esversion: 6 */
 
 
+ Array.prototype.random = function (length) {
+       return this[Math.floor((Math.random()*length))];
+ }
+
 module.exports = function (robot) {
+  facts = require('../facts.json');
+  factArr = facts.facts;
   robot.respond(/(armadillo)(\s)?(me)?/i, function(msg) {
-     msg.send("Fact goes here");
+     msg.send(factArr.random( factArr.length));
   });
 }
+
 
 
